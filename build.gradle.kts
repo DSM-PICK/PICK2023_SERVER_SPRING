676b501ee3caa147294dcdc22cee003d3733ed48
lyutvs <lyutvs@gmail.com>
2023-01-19 18:05:45 +0900

    ♻️ :: 의존관계 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,88 @@
 plugins {
-	kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version PluginVersions.JVM_VERSION
 }
 
 subprojects {
-	apply {
-		plugin("org.jetbrains.kotlin.jvm")
-		version = "1.6.21"
-	}
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        version = PluginVersions.JVM_VERSION
+    }
 
-	dependencies {
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	}
+    apply {
+        plugin("org.jetbrains.kotlin.kapt")
+        version = PluginVersions.KAPT_VERSION
+    }
+
+    dependencies {
+        implementation(Dependencies.KOTLIN_REFLECT)
+        implementation(Dependencies.KOTLIN_STDLIB)
+    }
 }
 
 allprojects {
-	group = "com.pick-dsm"
-	version = "0.0.1-SNAPSHOT"
+    group = "com.pickdsm"
+    version = "0.0.1-SNAPSHOT"
 
-	tasks {
-		compileKotlin {
-			kotlinOptions {
-				freeCompilerArgs = listOf("-Xjsr305=strict")
-				jvmTarget = "17"
-			}
-		}
+    tasks {
+        compileKotlin {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "17"
+            }
+        }
 
-		compileJava {
-			sourceCompatibility = JavaVersion.VERSION_17.majorVersion
-		}
+        compileJava {
+            sourceCompatibility = JavaVersion.VERSION_17.majorVersion
+        }
 
-		test {
-			useJUnitPlatform()
-		}
-	}
+        test {
+            useJUnitPlatform()
+        }
+    }
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
+}
+
+val ktlint: Configuration by configurations.creating
+
+dependencies {
+    ktlint(Dependencies.KTLINT) {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
+}
+
+val outputDir = "${project.buildDir}/reports/ktlint/"
+val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+
+val ktlintCheck by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    args = listOf("**/*.kt", "**/*.kts")
+}
+
+// Formatting all source files
+val ktlintFormat by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    args = listOf("-F", "**/*.kt")
+    jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
--- buildSrc/src/main/kotlin/PluginVersions.kt
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -3,11 +3,11 @@ object PluginVersions {
     const val DEPENDENCY_MANAGER_VERSION = "1.0.15.RELEASE"
     const val JVM_VERSION = "1.6.10"
     const val SPRING_PLUGIN_VERSION = "1.6.10"
-    const val JPA_PLUGIN_VERSION = "1.6.10"
     const val KAPT_VERSION = "1.6.10"
     const val REACTIVE_MYSQL_VERSION = "4.2.5"
     const val EMBEDDED_MYSQL_VERSION = "4.6.2"
     const val HIBERNATE_REACTIVE_VERSION = "1.1.3.Final"
     const val MUTINY_VERSION = "1.4.0"
     const val JDSL_VERSION = "2.0.1.RELEASE"
+    const val JPA_PLUGIN_VERSION = "1.6.10"
 }
