232a9e2a4496f06e3fdb23b1036583133786d301
min56 <minchaeg85@gmail.com>
2023-01-19 23:30:48 +0900

    ⚡️ :: UUID7로 버전 변경

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/global/entity/BaseUUIDEntity.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/global/entity/BaseUUIDEntity.kt
@@ -1,17 +1,16 @@
 package com.pickdsm.pickserverspring.global.entity
 
-import org.hibernate.annotations.GenericGenerator
-import java.util.UUID
+import com.github.f4b6a3.uuid.UuidCreator
+import java.util.*
 import javax.persistence.Column
-import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 
+
 @MappedSuperclass
 abstract class BaseUUIDEntity (
+
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
-    val id: UUID = UUID(0,0)
+    @Column(columnDefinition = "BINARY(16)", nullable = false)
+    val id: UUID = UuidCreator.getTimeOrderedEpoch()
 )
