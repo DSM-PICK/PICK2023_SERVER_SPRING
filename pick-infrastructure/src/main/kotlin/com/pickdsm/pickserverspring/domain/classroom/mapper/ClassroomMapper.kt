7a480669722ade177c2296a60bad56c1b16e22a6
min56 <minchaeg85@gmail.com>
2023-01-20 00:08:41 +0900

    📑 :: ClassroomMaper 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/mapper/ClassroomMapper.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/mapper/ClassroomMapper.kt
@@ -2,24 +2,10 @@ package com.pickdsm.pickserverspring.domain.classroom.mapper
 
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
 import com.pickdsm.pickserverspring.domain.classroom.persistence.entity.ClassroomEntity
-import org.springframework.stereotype.Component
 
-@Component
-class ClassroomMapper {
+interface ClassroomMapper {
 
-    fun domainToEntity(classroom: Classroom): ClassroomEntity {
-        return ClassroomEntity(
-            id = classroom.id,
-            name = classroom.name,
-            floor = classroom.floor
-        )
-    }
+    fun domainToEntity(classroom: Classroom): ClassroomEntity
 
-    fun entityToDomain(classroomEntity: ClassroomEntity): Classroom {
-        return Classroom(
-            id = classroomEntity.id,
-            name = classroomEntity.name,
-            floor = classroomEntity.floor
-        )
-    }
+    fun entityToDomain(classroomEntity: ClassroomEntity): Classroom
 }
