ee77778e130a98c387730387ae08921ae679fbb0
min56 <minchaeg85@gmail.com>
2023-01-20 20:24:36 +0900

    ♻️ :: 공백 추가

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/error/StatusErrorCode.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/error/StatusErrorCode.kt
@@ -4,10 +4,10 @@ import com.pickdsm.pickserverspring.common.error.ErrorProperty
 
 enum class StatusErrorCode(
     private val status: Int,
-    private val message: String
+    private val message: String,
 ) : ErrorProperty {
 
-    STATUS_NOT_FOUND(404, "Status not found"),;
+    STATUS_NOT_FOUND(404, "Status not found"), ;
 
     override fun status(): Int = status
     override fun message(): String = message
