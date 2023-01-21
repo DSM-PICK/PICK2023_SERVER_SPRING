53d5d57c75850a873643b7af701d01531b299290
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-21 17:29:09 +0900

    📑 :: SelfStudyDirectorEntity 추가

---
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/persistence/entity/SelfStudyDirectorEntity.kt
@@ -0,0 +1,30 @@
+package com.pickdsm.pickserverspring.domain.selfstudydirector.persistence.entity
+
+import com.pickdsm.pickserverspring.domain.selfstudydirector.DirectorType
+import com.pickdsm.pickserverspring.global.entity.BaseUUIDEntity
+import org.hibernate.annotations.ColumnDefault
+import java.time.LocalDate
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Table(name = "tbl_selfstudy_director")
+@Entity
+class SelfStudyDirectorEntity(
+
+    override val id: UUID,
+
+    @Column(nullable = false)
+    val floor: Int,
+
+    @Column(columnDefinition = "BINARY(16)", nullable = false)
+    val teacherId: UUID,
+
+    @Column(columnDefinition = "DATE", nullable = false)
+    val date: LocalDate,
+
+    @Column(columnDefinition = "VARCHAR(12)", nullable = false)
+    @ColumnDefault("''")
+    val type: DirectorType,
+) : BaseUUIDEntity()
