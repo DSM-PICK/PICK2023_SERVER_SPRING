00baf27933bd4c100aa41654d0bd33a18fb05f32
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-25 12:07:18 +0900

    ♻️ :: application.studentId로 수정

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/mapper/ApplicationMapperImpl.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/mapper/ApplicationMapperImpl.kt
@@ -1,23 +1,16 @@
 package com.pickdsm.pickserverspring.domain.application.mapper
 
 import com.pickdsm.pickserverspring.domain.application.Application
-import com.pickdsm.pickserverspring.domain.application.exception.StatusNotFoundException
-import com.pickdsm.pickserverspring.domain.application.persistence.StatusRepository
 import com.pickdsm.pickserverspring.domain.application.persistence.entity.ApplicationEntity
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
-class ApplicationMapperImpl(
-    private val statusRepository: StatusRepository,
-) : ApplicationMapper {
+class ApplicationMapperImpl : ApplicationMapper {
 
     override fun domainToEntity(application: Application): ApplicationEntity {
-        val statusEntity = statusRepository.findByIdOrNull(application.id) ?: throw StatusNotFoundException
-
         return ApplicationEntity(
             id = application.id,
-            studentId = statusEntity.studentId,
+            studentId = application.studentId,
             date = application.date,
             startTime = application.startTime,
             endTime = application.endTime,
