470c721e626f55fc8c455ab047b0bdfd33257de0
jaemin <sonjm3518@dsm.hs.kr>
2023-01-25 22:34:21 +0900

    ⚡ :: Id로 Entity 찾기

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/ClassroomRepository.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/ClassroomRepository.kt
@@ -1,9 +1,12 @@
 package com.pickdsm.pickserverspring.domain.classroom.persistence
 
+import com.pickdsm.pickserverspring.domain.classroom.Classroom
 import com.pickdsm.pickserverspring.domain.classroom.persistence.entity.ClassroomEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface ClassroomRepository : CrudRepository<ClassroomEntity, UUID>
+interface ClassroomRepository : CrudRepository<ClassroomEntity, UUID> {
+    fun findClassroomEntityById(classroomId: UUID) : Classroom?
+}
