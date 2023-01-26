fc3fc76c2707115f3fcb24cba690967d102898c2
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-26 12:37:46 +0900

    📑 :: ApplicationWebAdapter -> getPicnnicApplicationList 메소드 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/presentation/ApplicationWebAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/presentation/ApplicationWebAdapter.kt
@@ -1,23 +1,30 @@
 package com.pickdsm.pickserverspring.domain.application.presentation
 
+import com.pickdsm.pickserverspring.domain.application.api.QueryPicnicApplicationListApi
+import com.pickdsm.pickserverspring.domain.application.api.dto.response.QueryPicnicApplicationList
 import com.pickdsm.pickserverspring.domain.classroom.api.ClassroomMovementApi
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RequestMapping("/applications")
 @RestController
 class ApplicationWebAdapter(
     private val classroomMovementApi: ClassroomMovementApi,
+    private val queryPicnicApplicationListApi: QueryPicnicApplicationListApi,
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{classroom-id}")
     fun saveClassMovement(@PathVariable("classroom-id") classRoomId: UUID) {
         classroomMovementApi.saveClassroomMovement(classRoomId)
+    }
+
+    @GetMapping
+    fun getPicnicApplicationList(
+        @RequestParam grade: Int?,
+        @RequestParam classNum: Int?,
+    ): QueryPicnicApplicationList {
+        return queryPicnicApplicationListApi.getPicnicApplicationList(grade, classNum)
     }
 }
