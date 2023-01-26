4e51ff206d3df6f1709eac2fd701e48e08c9c4cd
min56 <minchaeg85@gmail.com>
2023-01-26 21:24:48 +0900

    ⚡️ :: queryClassroomListByFloor 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/adapter/ClassPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/adapter/ClassPersistenceAdapter.kt
@@ -1,11 +1,12 @@
 package com.pickdsm.pickserverspring.domain.classroom.persistence.adapter
 
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
 import com.pickdsm.pickserverspring.domain.classroom.exception.ClassroomNotFoundException
 import com.pickdsm.pickserverspring.domain.classroom.persistence.ClassroomRepository
 import com.pickdsm.pickserverspring.domain.classroom.spi.ClassroomSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
-import java.util.UUID
+import java.util.*
 
 @Adapter
 class ClassPersistenceAdapter(
@@ -14,4 +15,17 @@ class ClassPersistenceAdapter(
 
     override fun queryClassroomById(classroomId: UUID): Classroom =
         classroomRepository.findClassroomEntityById(classroomId) ?: throw ClassroomNotFoundException
+
+    override fun queryClassroomListByFloor(floor: Int): List<ClassroomElement> {
+        val classroomList = classroomRepository.findClassroomEntityByFloor(floor)
+
+        return classroomList
+            .map {
+                classroom -> ClassroomElement(
+                    id = classroom.id,
+                    name = classroom.name,
+                )
+            }
+    }
+
 }
