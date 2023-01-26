4410c045e71039e86f1da631b2fd96b515730fe0
min56 <minchaeg85@gmail.com>
2023-01-26 21:23:11 +0900

    🐛 :: Conflict 해결

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/ClassroomSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/ClassroomSpi.kt
@@ -1,8 +1,4 @@
 package com.pickdsm.pickserverspring.domain.classroom.spi
 
-<<<<<<< Updated upstream
-interface ClassroomSpi : QueryClassroomSpi
-=======
 interface ClassroomSpi : QueryClassroomSpi {
 }
->>>>>>> Stashed changes
