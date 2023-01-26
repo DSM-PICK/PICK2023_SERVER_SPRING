348881dc256044af003b84fe9ce4b295c4bb465e
min56 <minchaeg85@gmail.com>
2023-01-26 21:28:50 +0900

    📑 :: ClassroomWebAdapter

---
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/presentation/ClassroomWebAdapter.kt
@@ -0,0 +1,20 @@
+package com.pickdsm.pickserverspring.domain.classroom.presentation
+
+import com.pickdsm.pickserverspring.domain.classroom.api.QueryClassroomListApi
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.QueryClassroomList
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/applications")
+@RestController
+class ClassroomWebAdapter (
+    private val classroomApi: QueryClassroomListApi,
+) {
+
+    @GetMapping("/")
+    fun getClassroomList(@RequestParam floor: Int): QueryClassroomList {
+        return classroomApi.queryClassroomList(floor)
+    }
+}
