9aaf7518c70e2b6b7b2fc700cc0ac300f81198ac
정대현 <eogus_2513@dsm.hs.kr>
2023-01-26 15:42:41 +0900

    ♻️ :: Ktlint 수정

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/uuid/CustomUUIDGenerator.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/uuid/CustomUUIDGenerator.kt
@@ -4,11 +4,11 @@ import com.github.f4b6a3.uuid.UuidCreator
 import org.hibernate.engine.spi.SharedSessionContractImplementor
 import org.hibernate.id.IdentifierGenerator
 import java.io.Serializable
-import java.util.UUID
 
-class UUIDv7Generator : IdentifierGenerator {
+class CustomUUIDGenerator : IdentifierGenerator {
 
     override fun generate(
-        session: SharedSessionContractImplementor, entity: Any
-    ): Serializable = UuidCreator.getTimeOrderedEpoch()
+        session: SharedSessionContractImplementor,
+        entity: Any
+    ) : Serializable = UuidCreator.getTimeOrderedEpoch()
 }
