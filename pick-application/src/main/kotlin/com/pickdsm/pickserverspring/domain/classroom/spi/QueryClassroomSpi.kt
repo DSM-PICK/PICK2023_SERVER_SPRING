c99df95d28da3328d8ce0ece4d5c7a9cef36cd45
min56 <minchaeg85@gmail.com>
2023-01-26 21:22:16 +0900

    ⚡️ :: queryClassroomListByFloor 추가

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/QueryClassroomSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/QueryClassroomSpi.kt
@@ -1,17 +1,12 @@
 package com.pickdsm.pickserverspring.domain.classroom.spi
 
-<<<<<<< Updated upstream
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.QueryClassroomList
 import java.util.UUID
 
 interface QueryClassroomSpi {
     fun queryClassroomById(classroomId: UUID): Classroom
-}
-=======
-import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
 
-interface QueryClassroomSpi {
-
-    fun queryClassroomList(floor: Int): List<ClassroomElement>
+    fun queryClassroomListByFloor(floor: Int): List<ClassroomElement>
 }
->>>>>>> Stashed changes
