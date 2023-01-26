0c0feac2cb7b6a830567ca16e32398a7ac14d968
min56 <minchaeg85@gmail.com>
2023-01-26 21:25:52 +0900

    ⚡️ :: findClassroomEntityByFloor 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/ClassroomRepository.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/ClassroomRepository.kt
@@ -1,6 +1,8 @@
 package com.pickdsm.pickserverspring.domain.classroom.persistence
 
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.QueryClassroomList
 import com.pickdsm.pickserverspring.domain.classroom.persistence.entity.ClassroomEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
@@ -9,4 +11,6 @@ import java.util.UUID
 @Repository
 interface ClassroomRepository : CrudRepository<ClassroomEntity, UUID> {
     fun findClassroomEntityById(classroomId: UUID): Classroom?
+
+    fun findClassroomEntityByFloor(floor: Int): List<ClassroomElement>
 }
