37d3d46996326b3f3926700d8b3403ef2b248cc1
min56 <minchaeg85@gmail.com>
2023-01-26 21:23:51 +0900

    📑 :: QueryClassroomListUseCase

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/usecase/QueryClassroomListUseCase.kt
@@ -0,0 +1,28 @@
+package com.pickdsm.pickserverspring.domain.classroom.usecase
+
+import com.pickdsm.pickserverspring.common.annotation.ReadOnlyUseCase
+import com.pickdsm.pickserverspring.domain.classroom.api.QueryClassroomListApi
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.QueryClassroomList
+import com.pickdsm.pickserverspring.domain.classroom.spi.QueryClassroomSpi
+
+@ReadOnlyUseCase
+class QueryClassroomListUseCase(
+    private val queryClassroomSpi: QueryClassroomSpi,
+) : QueryClassroomListApi {
+
+    override fun queryClassroomList(floor: Int): QueryClassroomList {
+        val classroomList = queryClassroomSpi.queryClassroomListByFloor(floor)
+
+        return QueryClassroomList(
+           classroomList = classroomList
+                .map {
+                    classroom -> ClassroomElement(
+                        id = classroom.id,
+                        name = classroom.name,
+                    )
+                }
+        )
+    }
+
+}
