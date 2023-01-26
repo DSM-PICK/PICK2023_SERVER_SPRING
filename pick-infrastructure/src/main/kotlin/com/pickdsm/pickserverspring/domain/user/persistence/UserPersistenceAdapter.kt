671685a8dacaa2106eb64f78774bd0e50d49b814
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-26 12:34:57 +0900

    📑 :: UserPersistenceAdpater -> 유저 정보 요청 메소드 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/user/persistence/UserPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/user/persistence/UserPersistenceAdapter.kt
@@ -1,13 +1,33 @@
 package com.pickdsm.pickserverspring.domain.user.persistence
 
+import com.pickdsm.pickserverspring.common.feign.client.UserClient
+import com.pickdsm.pickserverspring.domain.user.dto.UserInfo
+import com.pickdsm.pickserverspring.domain.user.spi.ApplicationUserSpi
 import com.pickdsm.pickserverspring.domain.user.spi.UserSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
 import org.springframework.security.core.context.SecurityContextHolder
-import java.util.UUID
+import java.util.*
 
 @Adapter
-class UserPersistenceAdapter : UserSpi {
+class UserPersistenceAdapter(
+    private val userClient: UserClient,
+) : UserSpi, ApplicationUserSpi {
 
     override fun getCurrentUserId(): UUID =
         UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+
+    override fun queryUserInfo(ids: List<UUID>): List<UserInfo> {
+        val userList = userClient.getUserInfo(ids).userList
+
+        return userList
+            .map {
+                UserInfo(
+                    id = it.id,
+                    grade = it.grade,
+                    classNum = it.classNum,
+                    num = it.num,
+                    studentName = it.name,
+                )
+            }
+    }
 }
