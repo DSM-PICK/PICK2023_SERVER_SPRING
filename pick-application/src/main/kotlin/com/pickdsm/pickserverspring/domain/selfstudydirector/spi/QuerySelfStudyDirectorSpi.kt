9cf049b45035a6b26be391891475ef1bf75e660e
정대현 <eogus_2513@dsm.hs.kr>
2023-01-27 22:49:20 +0900

    📑 :: QuerySelfStudyDirectorSpi

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/spi/QuerySelfStudyDirectorSpi.kt
@@ -0,0 +1,12 @@
+package com.pickdsm.pickserverspring.domain.selfstudydirector.spi
+
+import com.pickdsm.pickserverspring.domain.selfstudydirector.SelfStudyDirector
+import java.time.LocalDate
+import java.util.UUID
+
+interface QuerySelfStudyDirectorSpi {
+
+    fun querySelfStudyDirectorByDate(date: LocalDate): List<SelfStudyDirector>
+
+    fun querySelfStudyDirectorTeacherIdByDate(date: LocalDate): List<UUID>
+}
