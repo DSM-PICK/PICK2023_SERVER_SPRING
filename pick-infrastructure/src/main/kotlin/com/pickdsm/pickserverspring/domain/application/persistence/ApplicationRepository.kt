7c788df9a3390911149620c662e0d1f9d4f851eb
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-27 09:56:27 +0900

    ⚡️ :: findAllByDate 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/ApplicationRepository.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/ApplicationRepository.kt
@@ -1,9 +1,14 @@
 package com.pickdsm.pickserverspring.domain.application.persistence
 
+import com.pickdsm.pickserverspring.domain.application.Application
 import com.pickdsm.pickserverspring.domain.application.persistence.entity.ApplicationEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 import java.util.UUID
 
 @Repository
-interface ApplicationRepository : CrudRepository<ApplicationEntity, UUID>
+interface ApplicationRepository : CrudRepository<ApplicationEntity, UUID> {
+
+    fun findAllByDate(date: LocalDate): List<Application>
+}
