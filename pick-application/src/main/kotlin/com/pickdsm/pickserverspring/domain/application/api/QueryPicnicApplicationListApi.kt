7fdbe7e9487628b6216059c1a5a99aca4e4edc41
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-27 14:06:39 +0900

    ♻️ :: Api 하나로 합치기

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/api/QueryPicnicApplicationListApi.kt
+++
@@ -1,8 +0,0 @@
-package com.pickdsm.pickserverspring.domain.application.api
-
-import com.pickdsm.pickserverspring.domain.application.api.dto.response.QueryPicnicApplicationList
-
-interface QueryPicnicApplicationListApi {
-
-    fun getPicnicApplicationListByGradeAndClassNum(grade: Int?, classNum: Int?): QueryPicnicApplicationList
-}
