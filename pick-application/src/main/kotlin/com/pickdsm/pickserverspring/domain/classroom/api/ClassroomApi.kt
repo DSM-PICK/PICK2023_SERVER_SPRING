3ace87fce2147cab3d5f1de0cf5d6dfbfe922473
min56 <minchaeg85@gmail.com>
2023-01-27 14:49:04 +0900

    ♻️ :: ClassroomApi로 변경

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/api/ClassroomApi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/api/ClassroomApi.kt
@@ -2,7 +2,7 @@ package com.pickdsm.pickserverspring.domain.classroom.api
 
 import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.QueryClassroomList
 
-interface QueryClassroomListApi {
+interface ClassroomApi {
 
     fun queryClassroomList(floor: Int): QueryClassroomList
 }
