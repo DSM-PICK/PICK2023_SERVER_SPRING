14356b3e68ccbf765c14c62df83ae2a32b618a0d
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-27 13:42:15 +0900

    ♻️ :: Q타입 static import로 수정

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/adapter/ApplicationPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/adapter/ApplicationPersistenceAdapter.kt
@@ -3,7 +3,7 @@ package com.pickdsm.pickserverspring.domain.application.persistence.adapter
 import com.pickdsm.pickserverspring.domain.application.Application
 import com.pickdsm.pickserverspring.domain.application.mapper.ApplicationMapper
 import com.pickdsm.pickserverspring.domain.application.persistence.ApplicationRepository
-import com.pickdsm.pickserverspring.domain.application.persistence.entity.QApplicationEntity
+import com.pickdsm.pickserverspring.domain.application.persistence.entity.QApplicationEntity.applicationEntity
 import com.pickdsm.pickserverspring.domain.application.spi.ApplicationSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
 import com.querydsl.jpa.impl.JPAQueryFactory
@@ -27,8 +27,8 @@ class ApplicationPersistenceAdapter(
 
     override fun queryAllStudentId(): List<UUID> {
         return jpaQueryFactory
-            .select(QApplicationEntity.applicationEntity.studentId)
-            .from(QApplicationEntity.applicationEntity)
+            .select(applicationEntity.studentId)
+            .from(applicationEntity)
             .fetch()
     }
 }
