79ba5287ee7f048887ffddeb8c0ab018bf60d24e
정대현 <eogus_2513@dsm.hs.kr>
2023-01-31 13:06:28 +0900

    ⚡️ :: Status Spi 추가

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/spi/StatusSpi.kt
@@ -0,0 +1,5 @@
+package com.pickdsm.pickserverspring.domain.application.spi
+
+import com.pickdsm.pickserverspring.domain.teacher.spi.StatusCommandTeacherSpi
+
+interface StatusSpi: StatusCommandTeacherSpi
