471d014cf43734552cb7418cb4bbb5de3e61174e
정대현 <eogus_2513@dsm.hs.kr>
2023-01-31 13:07:37 +0900

    ♻️ :: inner data class로 변경

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/feign/client/dto/response/UserInfoResponse.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/feign/client/dto/response/UserInfoResponse.kt
@@ -1,8 +1,32 @@
 package com.pickdsm.pickserverspring.common.feign.client.dto.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
 import com.pickdsm.pickserverspring.global.annotation.NoArg
+import java.time.LocalDate
+import java.util.*
 
 @NoArg
 class UserInfoResponse(
     val users: List<UserInfoElement>,
-)
+) {
+
+    @NoArg
+    class UserInfoElement(
+        val id: UUID,
+        @JsonProperty("account_id")
+        val accountId: String,
+        val password: String,
+        val name: String,
+        val grade: Int,
+        @JsonProperty("class_num")
+        val classNum: Int,
+        val num: Int,
+        @JsonProperty("birth_day")
+        @JsonDeserialize(using = LocalDateDeserializer::class)
+        val birthDay: LocalDate,
+        @JsonProperty("profile_file_name")
+        val profileFileName: String,
+    )
+}
