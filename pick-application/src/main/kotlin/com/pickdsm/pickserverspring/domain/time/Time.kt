5b17fd2dab700772e8ce399acd706691fac1ffee
정대현 <eogus_2513@dsm.hs.kr>
2023-01-31 16:22:18 +0900

    ♻️ :: id Int -> UUID로 타입 변경

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/time/Time.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/time/Time.kt
@@ -2,13 +2,14 @@ package com.pickdsm.pickserverspring.domain.time
 
 import java.time.LocalDate
 import java.time.LocalTime
+import java.util.UUID
 
 data class Time(
     val date: LocalDate,
     val timeList: List<DomainTimeElement>,
 ) {
     data class DomainTimeElement(
-        val id: Int,
+        val id: UUID,
         val period: Int,
         val startTime: LocalTime,
         val endTime: LocalTime,
--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/feign/client/dto/response/TimeResponse.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/feign/client/dto/response/TimeResponse.kt
@@ -3,6 +3,7 @@ package com.pickdsm.pickserverspring.common.feign.client.dto.response
 import com.pickdsm.pickserverspring.global.annotation.NoArg
 import java.time.LocalDate
 import java.time.LocalTime
+import java.util.UUID
 
 @NoArg
 data class TimeResponse(
@@ -12,7 +13,7 @@ data class TimeResponse(
 
     @NoArg
     data class TimeElement(
-        val id: Int,
+        val id: UUID,
         val period: Int,
         val startTime: LocalTime,
         val endTime: LocalTime,
