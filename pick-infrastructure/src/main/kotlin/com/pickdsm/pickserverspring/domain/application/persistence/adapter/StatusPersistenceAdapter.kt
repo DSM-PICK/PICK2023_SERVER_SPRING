9f321f3ea0dd9ce659c64efb321aa268732b354e
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-01 23:55:33 +0900

    📑 :: StatusPersistenceAdapter

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/adapter/StatusPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/adapter/StatusPersistenceAdapter.kt
@@ -3,13 +3,18 @@ package com.pickdsm.pickserverspring.domain.application.persistence.adapter
 import com.pickdsm.pickserverspring.domain.application.Status
 import com.pickdsm.pickserverspring.domain.application.mapper.StatusMapper
 import com.pickdsm.pickserverspring.domain.application.persistence.StatusRepository
+import com.pickdsm.pickserverspring.domain.application.persistence.entity.QStatusEntity.statusEntity
 import com.pickdsm.pickserverspring.domain.application.spi.StatusSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
+import com.querydsl.jpa.impl.JPAQueryFactory
+import java.time.LocalDate
+import java.util.UUID
 
 @Adapter
 class StatusPersistenceAdapter(
     private val statusMapper: StatusMapper,
     private val statusRepository: StatusRepository,
+    private val jpaQueryFactory: JPAQueryFactory,
 ) : StatusSpi {
 
     override fun saveAllStatus(statusList: List<Status>) {
@@ -17,5 +22,21 @@ class StatusPersistenceAdapter(
             statusMapper.domainToEntity(it)
         }
         statusRepository.saveAll(statusEntityList)
+    }
+
+    override fun queryPicnicStudentIdListByToday(date: LocalDate): List<UUID> {
+        return jpaQueryFactory
+            .select(statusEntity.studentId)
+            .from(statusEntity)
+            .where(statusEntity.date.eq(date))
+            .fetch()
+    }
+
+    override fun queryPicnicStudentInfoListByToday(date: LocalDate): List<Status> {
+        return jpaQueryFactory
+            .selectFrom(statusEntity)
+            .where(statusEntity.date.eq(date))
+            .fetch()
+            .map(statusMapper::entityToDomain)
     }
 }
