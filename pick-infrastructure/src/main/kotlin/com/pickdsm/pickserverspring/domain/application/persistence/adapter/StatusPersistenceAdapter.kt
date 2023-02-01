44dd623b4a2388e2a43666de7c0d509a14d055f9
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-01 11:52:57 +0900

    ♻️ :: 안 쓰는 메소드 제거

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/adapter/StatusPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/adapter/StatusPersistenceAdapter.kt
@@ -3,18 +3,13 @@ package com.pickdsm.pickserverspring.domain.application.persistence.adapter
 import com.pickdsm.pickserverspring.domain.application.Status
 import com.pickdsm.pickserverspring.domain.application.mapper.StatusMapper
 import com.pickdsm.pickserverspring.domain.application.persistence.StatusRepository
-import com.pickdsm.pickserverspring.domain.application.persistence.entity.QStatusEntity.statusEntity
-import com.pickdsm.pickserverspring.domain.application.persistence.entity.StatusEntity
 import com.pickdsm.pickserverspring.domain.application.spi.StatusSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
-import com.querydsl.jpa.impl.JPAQueryFactory
-import java.util.UUID
 
 @Adapter
 class StatusPersistenceAdapter(
     private val statusMapper: StatusMapper,
     private val statusRepository: StatusRepository,
-    private val jpaQueryFactory: JPAQueryFactory,
 ) : StatusSpi {
 
     override fun saveAllStatus(statusList: List<Status>) {
@@ -22,19 +17,5 @@ class StatusPersistenceAdapter(
             statusMapper.domainToEntity(it)
         }
         statusRepository.saveAll(statusEntityList)
-    }
-
-    override fun changeStatusToPicnic(statusIdList: List<UUID>) {
-        val statusList = statusRepository.findAllById(statusIdList)
-            .map(StatusEntity::changeStatusToPicnic)
-
-        statusRepository.saveAll(statusList)
-    }
-
-    override fun queryStatusIdList(): List<UUID> {
-        return jpaQueryFactory
-            .select(statusEntity.id)
-            .from(statusEntity)
-            .fetch()
     }
 }
