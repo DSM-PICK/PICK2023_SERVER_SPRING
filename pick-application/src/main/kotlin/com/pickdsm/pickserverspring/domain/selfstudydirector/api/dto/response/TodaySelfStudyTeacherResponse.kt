6285a5f99fb91499be7e8f20f7da50487354be69
lyutvs <lyutvs@gmail.com>
2023-02-01 16:06:33 +0900

    📑 :: TodatSelfStudyTeacherResponse

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/api/dto/response/TodaySelfStudyTeacherResponse.kt
@@ -0,0 +1,7 @@
+package com.pickdsm.pickserverspring.domain.selfstudydirector.api.dto.response
+
+data class TodaySelfStudyTeacherResponse(
+    val secondFloor: String,
+    val thirdFloor: String,
+    val fourthFloor: String
+)
