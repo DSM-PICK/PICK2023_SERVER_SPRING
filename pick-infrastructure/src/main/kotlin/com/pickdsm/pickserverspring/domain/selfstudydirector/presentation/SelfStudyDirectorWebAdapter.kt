cf60e32785ba1f7d4b7da3369412525e33607cb8
lyutvs <lyutvs@gmail.com>
2023-02-01 16:07:08 +0900

    ⚡️ :: SelfStudyDirectorWebAdaper

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/presentation/SelfStudyDirectorWebAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/presentation/SelfStudyDirectorWebAdapter.kt
@@ -2,6 +2,7 @@ package com.pickdsm.pickserverspring.domain.selfstudydirector.presentation
 
 import com.pickdsm.pickserverspring.domain.selfstudydirector.api.SelfStudyDirectorApi
 import com.pickdsm.pickserverspring.domain.selfstudydirector.api.dto.response.SelfStudyListResponse
+import com.pickdsm.pickserverspring.domain.selfstudydirector.api.dto.response.TodaySelfStudyTeacherResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -18,4 +19,8 @@ class SelfStudyDirectorWebAdapter(
     fun getSelfStudyDirector(@RequestParam("month") month: String): SelfStudyListResponse {
         return selfStudyDirectorApi.getSelfStudyTeacher(month)
     }
+
+    @GetMapping("/today")
+    fun getTodaySelfStudyDirector(): TodaySelfStudyTeacherResponse =
+        selfStudyDirectorApi.getTodaySelfStudyTeacher()
 }
