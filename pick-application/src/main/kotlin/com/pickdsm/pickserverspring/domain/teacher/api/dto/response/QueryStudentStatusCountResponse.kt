78835830567971dd22fc602b111b294a58ac9973
jaemin <sonjm3518@dsm.hs.kr>
2023-02-02 22:03:41 +0900

    📑 :: QueryStudentStatusCountResponse

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/teacher/api/dto/response/QueryStudentStatusCountResponse.kt
@@ -0,0 +1,7 @@
+package com.pickdsm.pickserverspring.domain.teacher.api.dto.response
+
+data class QueryStudentStatusCountResponse(
+    val picnic: Int,
+    val application: Int,
+    val classroomMovement: Int,
+)
