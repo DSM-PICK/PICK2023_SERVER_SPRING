388b0a9af9684ebe88f0d17bf492a17b3259b01b
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-02 14:34:06 +0900

    ♻️ :: QueryStatusSpi -> 안 쓰는 메소드 제거

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/spi/QueryStatusSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/spi/QueryStatusSpi.kt
@@ -2,10 +2,7 @@ package com.pickdsm.pickserverspring.domain.application.spi
 
 import com.pickdsm.pickserverspring.domain.application.Status
 import java.time.LocalDate
-import java.util.UUID
 
 interface QueryStatusSpi {
-    fun queryPicnicStudentIdListByToday(date: LocalDate): List<UUID>
-
     fun queryPicnicStudentInfoListByToday(date: LocalDate): List<Status>
 }
