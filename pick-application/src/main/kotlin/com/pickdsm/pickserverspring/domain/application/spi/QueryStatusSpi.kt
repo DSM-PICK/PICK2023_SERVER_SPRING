e0a82f5fd5bcccbc88017743f4cc978198c9f782
jaemin <sonjm3518@dsm.hs.kr>
2023-02-02 22:21:11 +0900

    ♻️ :: 컴플리트

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/spi/QueryStatusSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/spi/QueryStatusSpi.kt
@@ -1,12 +1,11 @@
 package com.pickdsm.pickserverspring.domain.application.spi
 
 import com.pickdsm.pickserverspring.domain.application.Status
+import java.time.LocalDate
 
 interface QueryStatusSpi {
 
     fun getAllPicnicStatus(): List<Status>
-import java.time.LocalDate
 
-interface QueryStatusSpi {
     fun queryPicnicStudentInfoListByToday(date: LocalDate): List<Status>
 }
