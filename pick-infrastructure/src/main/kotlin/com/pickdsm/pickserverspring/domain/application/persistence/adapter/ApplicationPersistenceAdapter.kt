e76b5720cd87140385744ebdd512910f3e735c2a
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-07 11:28:31 +0900

    📑 :: ApplicationPersistenceAdapter

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/adapter/ApplicationPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/adapter/ApplicationPersistenceAdapter.kt
@@ -1,44 +1,54 @@
 package com.pickdsm.pickserverspring.domain.application.persistence.adapter
 
 import com.pickdsm.pickserverspring.domain.application.Application
+import com.pickdsm.pickserverspring.domain.application.exception.StatusNotFoundException
 import com.pickdsm.pickserverspring.domain.application.mapper.ApplicationMapper
 import com.pickdsm.pickserverspring.domain.application.persistence.ApplicationRepository
+import com.pickdsm.pickserverspring.domain.application.persistence.StatusRepository
 import com.pickdsm.pickserverspring.domain.application.persistence.entity.ApplicationEntity
 import com.pickdsm.pickserverspring.domain.application.persistence.entity.QApplicationEntity.applicationEntity
 import com.pickdsm.pickserverspring.domain.application.spi.ApplicationSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.repository.findByIdOrNull
 import java.time.LocalDate
 import java.util.UUID
 
 @Adapter
 class ApplicationPersistenceAdapter(
     private val jpaQueryFactory: JPAQueryFactory,
+    private val statusRepository: StatusRepository,
     private val applicationMapper: ApplicationMapper,
     private val applicationRepository: ApplicationRepository,
 ) : ApplicationSpi {
 
-    override fun saveApplication(application: Application) {
-        applicationRepository.save(applicationMapper.domainToEntity(application))
+    override fun saveApplication(application: Application, statusId: UUID) {
+        val statusEntity = statusRepository.findByIdOrNull(statusId)
+            ?: throw StatusNotFoundException
+
+        applicationRepository.save(
+            ApplicationEntity(
+                statusId = statusId,
+                statusEntity = statusEntity,
+                desiredStartTime = application.desiredStartTime,
+                desiredEndTime = application.desiredEndTime,
+                reason = application.reason,
+            )
+        )
     }
 
     override fun queryPicnicApplicationListByToday(date: LocalDate): List<Application> {
         return jpaQueryFactory
             .selectFrom(applicationEntity)
-            .where(applicationEntity.date.eq(date))
+            .where(applicationEntity.statusEntity.date.eq(date))
             .fetch()
             .map(applicationMapper::entityToDomain)
-    }
-
-    override fun changePermission(applicationIdList: List<UUID>) {
-        applicationRepository.findAllById(applicationIdList)
-            .map(ApplicationEntity::changePermission)
     }
 
     override fun queryApplicationListByToday(date: LocalDate): List<Application> {
         return jpaQueryFactory
             .selectFrom(applicationEntity)
-            .where(applicationEntity.date.eq(date))
+            .where(applicationEntity.statusEntity.date.eq(date))
             .fetch()
             .map(applicationMapper::entityToDomain)
     }
