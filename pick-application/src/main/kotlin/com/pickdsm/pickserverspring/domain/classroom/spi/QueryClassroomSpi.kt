25f3ac3968717e47dca11d266950ec5c5f029d9a
min56 <minchaeg85@gmail.com>
2023-02-07 23:11:29 +0900

    ⚡️ :: queryResponsibleClassroomListByFloor

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/QueryClassroomSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/QueryClassroomSpi.kt
@@ -2,10 +2,13 @@ package com.pickdsm.pickserverspring.domain.classroom.spi
 
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
 import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.QueryClassroomList
 import java.util.UUID
 
 interface QueryClassroomSpi {
     fun queryClassroomById(classroomId: UUID): Classroom
 
     fun queryClassroomListByFloor(floor: Int): List<ClassroomElement>
+
+    fun queryResponsibleClassroomListByFloor(floor: Int?): List<ClassroomElement>
 }
