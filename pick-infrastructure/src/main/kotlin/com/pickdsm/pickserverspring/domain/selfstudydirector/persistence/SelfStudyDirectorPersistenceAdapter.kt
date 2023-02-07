88c6bd1109676ab2e76daeae6df7d34405538a75
min56 <minchaeg85@gmail.com>
2023-02-07 23:14:30 +0900

    ⚡️ :: queryResponsibleFloorByTeacherId 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/persistence/SelfStudyDirectorPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/persistence/SelfStudyDirectorPersistenceAdapter.kt
@@ -1,12 +1,17 @@
 package com.pickdsm.pickserverspring.domain.selfstudydirector.persistence
 
 import com.pickdsm.pickserverspring.common.feign.client.UserClient
+import com.pickdsm.pickserverspring.domain.selfstudydirector.DirectorType
 import com.pickdsm.pickserverspring.domain.selfstudydirector.SelfStudyDirector
 import com.pickdsm.pickserverspring.domain.selfstudydirector.mapper.SelfStudyDirectorMapper
 import com.pickdsm.pickserverspring.domain.selfstudydirector.persistence.entity.QSelfStudyDirectorEntity.selfStudyDirectorEntity
 import com.pickdsm.pickserverspring.domain.selfstudydirector.spi.SelfStudyDirectorSpi
+import com.pickdsm.pickserverspring.domain.user.exception.UserNotFoundException
 import com.pickdsm.pickserverspring.global.annotation.Adapter
+import com.querydsl.jpa.JPAExpressions.select
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.apache.catalina.User
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
 import java.time.LocalDate
 import java.util.UUID
 
@@ -31,4 +36,13 @@ class SelfStudyDirectorPersistenceAdapter(
             .from(selfStudyDirectorEntity)
             .where(selfStudyDirectorEntity.date.between(date, date.plusMonths(1)))
             .fetch()
+
+    override fun queryResponsibleFloorByTeacherId(teacherId: UUID): Int? {
+
+        return jpaQueryFactory
+            .select(selfStudyDirectorEntity.floor)
+            .from(selfStudyDirectorEntity)
+            .where(selfStudyDirectorEntity.teacherId.eq(teacherId))
+            .fetchOne()
+    }
 }
