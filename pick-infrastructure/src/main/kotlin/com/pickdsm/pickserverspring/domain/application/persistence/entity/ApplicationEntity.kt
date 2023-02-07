da3db608d1687413478c02103389bec437ec89ae
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-07 11:26:22 +0900

    ♻️ :: ApplicationEntity -> ERD대로 변경

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/entity/ApplicationEntity.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/persistence/entity/ApplicationEntity.kt
@@ -1,53 +1,29 @@
 package com.pickdsm.pickserverspring.domain.application.persistence.entity
 
-import com.pickdsm.pickserverspring.global.entity.BaseUUIDEntity
 import org.hibernate.annotations.ColumnDefault
-import java.time.LocalDate
 import java.time.LocalTime
 import java.util.UUID
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Table
+import javax.persistence.*
 
 @Table(name = "tbl_application")
 @Entity
 class ApplicationEntity(
 
-    override val id: UUID,
+    @Id
+    val statusId: UUID,
 
-    @Column(columnDefinition = "BINARY(16)", nullable = false)
-    val studentId: UUID,
-
-    @Column(columnDefinition = "DATE", nullable = false)
-    val date: LocalDate = LocalDate.now(),
-
-    @Column(columnDefinition = "TIME", nullable = false)
-    val startTime: LocalTime,
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "status_id", columnDefinition = "BINARY(16)")
+    val statusEntity: StatusEntity,
 
     @Column(columnDefinition = "TIME", nullable = false)
-    val endTime: LocalTime,
+    val desiredStartTime: LocalTime,
+
+    @Column(columnDefinition = "TIME", nullable = false)
+    val desiredEndTime: LocalTime,
 
     @Column(columnDefinition = "VARCHAR(255)", nullable = false)
     @ColumnDefault("''")
     val reason: String,
-
-    isStatus: Boolean,
-
-    isPermission: Boolean,
-) : BaseUUIDEntity(id) {
-
-    @Column(columnDefinition = "TINYINT(1)", nullable = false)
-    @ColumnDefault("0")
-    var isStatus = isStatus
-        protected set
-
-    @Column(columnDefinition = "TINYINT(1)", nullable = false)
-    @ColumnDefault("0")
-    var isPermission = isPermission
-        protected set
-
-    fun changePermission(): ApplicationEntity {
-        this.isPermission = true
-        return this
-    }
-}
+)
