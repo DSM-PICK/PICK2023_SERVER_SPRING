050177de4e2e31f4e53b7559839812339ce539be
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-08 22:30:30 +0900

    ♻️ :: @NoArg 제거

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/feign/client/dto/response/UserResponse.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/feign/client/dto/response/UserResponse.kt
@@ -1,15 +1,12 @@
 package com.pickdsm.pickserverspring.common.feign.client.dto.response
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.pickdsm.pickserverspring.global.annotation.NoArg
-import java.util.*
+import java.util.UUID
 
-@NoArg
 data class UserResponse(
     val response: List<UserElement>,
 ) {
 
-    @NoArg
     data class UserElement(
         val id: UUID,
         @JsonProperty("profile_file_name")
