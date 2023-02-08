199c43a0771237e2e1a81abd7019d21fa029ec67
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-08 15:18:49 +0900

    📑 :: UserQueryTeacherSpi

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/teacher/spi/UserQueryTeacherSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/teacher/spi/UserQueryTeacherSpi.kt
@@ -1,9 +1,12 @@
 package com.pickdsm.pickserverspring.domain.teacher.spi
 
 import com.pickdsm.pickserverspring.domain.user.User
+import com.pickdsm.pickserverspring.domain.user.dto.UserInfo
 import java.util.UUID
 
 interface UserQueryTeacherSpi {
 
     fun queryUserInfo(ids: List<UUID>): List<User>
+
+    fun queryUserInfoByGradeAndClassNum(grade: Int, classNum: Int): List<UserInfo>
 }
