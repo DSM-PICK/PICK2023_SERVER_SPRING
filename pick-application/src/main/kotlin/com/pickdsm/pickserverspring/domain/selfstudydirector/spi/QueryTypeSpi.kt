6b177d9f653678e224acdbc8eddfaa3e022bd194
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-15 11:51:47 +0900

    📑 :: QueryTypeSpi

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/spi/QueryTypeSpi.kt
@@ -0,0 +1,11 @@
+package com.pickdsm.pickserverspring.domain.selfstudydirector.spi
+
+import com.pickdsm.pickserverspring.domain.selfstudydirector.Type
+import java.util.UUID
+
+interface QueryTypeSpi {
+
+    fun queryTypeListByToday(): List<Type>
+
+    fun queryTypeById(typeId: UUID): Type
+}
