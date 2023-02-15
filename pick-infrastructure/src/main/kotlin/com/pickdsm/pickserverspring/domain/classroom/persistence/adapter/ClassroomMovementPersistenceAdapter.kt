0769f100d0528e9b90ec60cc5a27dd8b86d7fde3
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-15 11:55:21 +0900

    📑 :: ClassroomMovementPersistenceAdapter

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/adapter/ClassroomMovementPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/adapter/ClassroomMovementPersistenceAdapter.kt
@@ -2,33 +2,22 @@ package com.pickdsm.pickserverspring.domain.classroom.persistence.adapter
 
 import com.pickdsm.pickserverspring.domain.application.Status
 import com.pickdsm.pickserverspring.domain.application.mapper.StatusMapper
-import com.pickdsm.pickserverspring.domain.classroom.Classroom
 import com.pickdsm.pickserverspring.domain.classroom.ClassroomMovement
-import com.pickdsm.pickserverspring.domain.classroom.mapper.ClassroomMapper
 import com.pickdsm.pickserverspring.domain.classroom.mapper.ClassroomMovementMapper
 import com.pickdsm.pickserverspring.domain.classroom.persistence.ClassroomMovementRepository
-import com.pickdsm.pickserverspring.domain.classroom.persistence.entity.ClassroomMovementEntity
 import com.pickdsm.pickserverspring.domain.classroom.spi.ClassroomMovementSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
 
 @Adapter
 class ClassroomMovementPersistenceAdapter(
     private val classroomMovementRepository: ClassroomMovementRepository,
-    private val classroomMapper: ClassroomMapper,
     private val statusMapper: StatusMapper,
     private val classroomMovementMapper: ClassroomMovementMapper,
 ) : ClassroomMovementSpi {
 
-    override fun saveClassroomMovement(status: Status, classroom: Classroom) {
-        val statusEntity = statusMapper.domainToEntity(status)
-        val classroomEntity = classroomMapper.domainToEntity(classroom)
-
+    override fun saveClassroomMovement(classroomMovement: ClassroomMovement) {
         classroomMovementRepository.save(
-            ClassroomMovementEntity(
-                statusId = statusEntity.id,
-                statusEntity = statusEntity,
-                classroomEntity = classroomEntity,
-            ),
+            classroomMovementMapper.domainToEntity(classroomMovement)
         )
     }
 
