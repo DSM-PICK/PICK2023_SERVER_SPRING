444d8cc18bbd34eb8829df0e29c1d065f4f31b3a
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-15 11:52:44 +0900

    ♻️ :: 안 쓰는 메소드 제거

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/ClassroomRepository.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/ClassroomRepository.kt
@@ -1,7 +1,6 @@
 package com.pickdsm.pickserverspring.domain.classroom.persistence
 
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
-import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
 import com.pickdsm.pickserverspring.domain.classroom.persistence.entity.ClassroomEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
@@ -9,7 +8,6 @@ import java.util.UUID
 
 @Repository
 interface ClassroomRepository : CrudRepository<ClassroomEntity, UUID> {
-    fun findClassroomEntityById(classroomId: UUID): Classroom?
 
-    fun findClassroomEntityByFloor(floor: Int): List<ClassroomElement>
+    fun findClassroomEntityById(classroomId: UUID): Classroom?
 }
