c5d8ec1863592e645eed48632237b22e8ae2c618
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-15 12:09:04 +0900

    ♻️ :: ktlint

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/persistence/entity/SelfStudyDirectorEntity.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/persistence/entity/SelfStudyDirectorEntity.kt
@@ -10,7 +10,6 @@ import javax.persistence.FetchType
 import javax.persistence.JoinColumn
 import javax.persistence.Table
 
-
 @Table(name = "tbl_selfstudy_director")
 @Entity
 class SelfStudyDirectorEntity(
