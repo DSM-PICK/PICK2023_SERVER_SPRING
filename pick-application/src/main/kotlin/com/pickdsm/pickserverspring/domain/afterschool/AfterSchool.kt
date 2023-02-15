7351087bb310423a03df63986c5f6407e46c75af
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-15 11:39:35 +0900

    📑 :: AfterSchool

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/afterschool/AfterSchool.kt
@@ -0,0 +1,18 @@
+package com.pickdsm.pickserverspring.domain.afterschool
+
+import com.pickdsm.pickserverspring.common.annotation.Aggregate
+import java.util.UUID
+
+@Aggregate
+class AfterSchool(
+
+    val id: UUID = UUID.randomUUID(),
+
+    val afterSchoolName: String,
+
+    val teacherId: UUID,
+
+    val studentId: UUID,
+
+    val classroomId: UUID,
+)
