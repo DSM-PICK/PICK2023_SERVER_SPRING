de6b59acc8e9ba000e8547ab48516d8be01a43ea
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-15 11:52:27 +0900

    📑 :: TypePersistenceAdapter

---
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/persistence/adapter/TypePersistenceAdapter.kt
@@ -0,0 +1,31 @@
+package com.pickdsm.pickserverspring.domain.selfstudydirector.persistence.adapter
+
+import com.pickdsm.pickserverspring.domain.selfstudydirector.Type
+import com.pickdsm.pickserverspring.domain.selfstudydirector.exception.TypeNotFoundException
+import com.pickdsm.pickserverspring.domain.selfstudydirector.mapper.TypeMapper
+import com.pickdsm.pickserverspring.domain.selfstudydirector.persistence.TypeRepository
+import com.pickdsm.pickserverspring.domain.selfstudydirector.persistence.entity.QTypeEntity.typeEntity
+import com.pickdsm.pickserverspring.domain.selfstudydirector.spi.TypeSpi
+import com.pickdsm.pickserverspring.global.annotation.Adapter
+import com.querydsl.jpa.impl.JPAQueryFactory
+import java.time.LocalDate
+import java.util.*
+
+@Adapter
+class TypePersistenceAdapter(
+    private val jpaQueryFactory: JPAQueryFactory,
+    private val typeMapper: TypeMapper,
+    private val typeRepository: TypeRepository,
+) : TypeSpi {
+
+    override fun queryTypeListByToday(): List<Type> =
+        jpaQueryFactory
+            .selectFrom(typeEntity)
+            .where(typeEntity.date.eq(LocalDate.now()))
+            .fetch()
+            .map(typeMapper::entityToDomain)
+
+    override fun queryTypeById(typeId: UUID): Type =
+        typeRepository.findTypeEntityById(typeId)
+            ?: throw TypeNotFoundException
+}
