a27903b08f49b8f1e16ba23e44b3101d5c56d645
lyutvs <lyutvs@gmail.com>
2023-02-16 16:25:54 +0900

    📑 :: AfterSchoolErrorCode

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/afterschool/error/AfterSchoolErrorCode.kt
@@ -0,0 +1,16 @@
+package com.pickdsm.pickserverspring.domain.afterschool.error
+
+import com.pickdsm.pickserverspring.common.error.ErrorProperty
+
+enum class AfterSchoolErrorCode(
+    private val status: Int,
+    private val message: String,
+) : ErrorProperty {
+
+    AFTER_SCHOOL_NOT_FOUND(404, "After School Not Found"),
+    AFTER_SCHOOL_STUDENT_NOT_FOUND(404, "After School Student Not Found"),
+    ;
+
+    override fun status() = status
+    override fun message() = message
+}
