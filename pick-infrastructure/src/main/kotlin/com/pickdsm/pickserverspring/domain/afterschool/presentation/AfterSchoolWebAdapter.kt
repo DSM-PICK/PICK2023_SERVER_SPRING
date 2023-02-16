c8bfb0aca3e482757fc05ecff4c44de72eed89b4
lyutvs <lyutvs@gmail.com>
2023-02-16 16:26:29 +0900

    📑 :: AfterSchoolWebAdapter

---
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/afterschool/presentation/AfterSchoolWebAdapter.kt
@@ -0,0 +1,29 @@
+package com.pickdsm.pickserverspring.domain.afterschool.presentation
+
+import com.pickdsm.pickserverspring.domain.afterschool.api.AfterSchoolApi
+import com.pickdsm.pickserverspring.domain.afterschool.api.dto.DomainDeleteAfterSchoolStudentRequest
+import com.pickdsm.pickserverspring.domain.afterschool.presentation.dto.requset.DeleteAfterSchoolStudentRequest
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RequestMapping("/admin")
+@RestController
+class AfterSchoolWebAdapter(
+    private val afterSchoolApi: AfterSchoolApi
+) {
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/")
+    fun deleteAfterSchoolStudent(@RequestBody @Valid requset: DeleteAfterSchoolStudentRequest) {
+        val domainRequset = DomainDeleteAfterSchoolStudentRequest(
+            afterSchoolId = requset.afterSchoolId,
+            studentId = requset.studentId
+        )
+        afterSchoolApi.deleteAfterSchoolStudent(domainRequset)
+    }
+}
