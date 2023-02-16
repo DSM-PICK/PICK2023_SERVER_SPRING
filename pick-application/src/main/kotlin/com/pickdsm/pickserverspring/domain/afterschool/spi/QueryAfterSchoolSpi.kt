0b05ac45900becbf5f487efba781b882d3e671b5
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-16 23:30:28 +0900

    📑 :: QueryAfterSchoolSpi

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/afterschool/spi/QueryAfterSchoolSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/afterschool/spi/QueryAfterSchoolSpi.kt
@@ -1,11 +1,12 @@
 package com.pickdsm.pickserverspring.domain.afterschool.spi
 
 import com.pickdsm.pickserverspring.domain.afterschool.AfterSchool
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
 import java.util.UUID
 
 interface QueryAfterSchoolSpi {
 
-    fun queryAfterSchoolList(): List<AfterSchool>
+    fun queryAfterSchoolClassroomListByFloor(floor: Int): List<ClassroomElement>
 
     fun findByAfterSchoolIdAndStudentId(afterSchoolId: UUID, studentId: UUID): AfterSchool?
 }
