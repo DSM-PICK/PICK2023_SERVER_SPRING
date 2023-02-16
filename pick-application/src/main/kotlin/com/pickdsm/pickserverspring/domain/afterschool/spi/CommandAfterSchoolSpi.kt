34bb9103ddcbeeac0bd70e031219bbc51bff9a0b
lyutvs <lyutvs@gmail.com>
2023-02-16 16:24:23 +0900

    📑 :: CommandAfterSchoilSpi

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/afterschool/spi/CommandAfterSchoolSpi.kt
@@ -0,0 +1,9 @@
+package com.pickdsm.pickserverspring.domain.afterschool.spi
+
+import com.pickdsm.pickserverspring.domain.afterschool.AfterSchool
+import java.util.UUID
+
+interface CommandAfterSchoolSpi {
+
+    fun deleteByAfterSchoolIdAndStudentId(afterSchoolId: UUID, studentId: UUID)
+}
