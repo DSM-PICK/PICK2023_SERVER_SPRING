ebb706f4f5018eeaee5ace1a2cffef95d89ccdd9
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-16 23:31:48 +0900

    📑 :: ClubPersistenceAdapter

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/club/persistence/adapter/ClubPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/club/persistence/adapter/ClubPersistenceAdapter.kt
@@ -1,7 +1,7 @@
 package com.pickdsm.pickserverspring.domain.club.persistence.adapter
 
-import com.pickdsm.pickserverspring.domain.club.Club
-import com.pickdsm.pickserverspring.domain.club.mapper.ClubMapper
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
+import com.pickdsm.pickserverspring.domain.classroom.persistence.entity.QClassroomEntity.classroomEntity
 import com.pickdsm.pickserverspring.domain.club.persistence.entity.QClubEntity.clubEntity
 import com.pickdsm.pickserverspring.domain.club.spi.ClubSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
@@ -9,13 +9,21 @@ import com.querydsl.jpa.impl.JPAQueryFactory
 
 @Adapter
 class ClubPersistenceAdapter(
-    private val clubMapper: ClubMapper,
     private val jpaQueryFactory: JPAQueryFactory,
 ) : ClubSpi {
 
-    override fun queryClubList(): List<Club> =
+    override fun queryClubClassroomListByFloor(floor: Int): List<ClassroomElement> =
         jpaQueryFactory
             .selectFrom(clubEntity)
+            .innerJoin(clubEntity.classroomEntity, classroomEntity)
+            .on(clubEntity.classroomEntity.id.eq(classroomEntity.id))
+            .where(clubEntity.classroomEntity.floor.eq(floor))
             .fetch()
-            .map(clubMapper::entityToDomain)
+            .map {
+                ClassroomElement(
+                    id = it.classroomEntity.id,
+                    name = it.classroomEntity.name,
+                    description = it.name,
+                )
+            }
 }
