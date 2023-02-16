85e41317c8720844c549cd73f4855f02e552d170
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-16 23:35:31 +0900

    📑 :: QueryClassroomSpi

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/QueryClassroomSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/QueryClassroomSpi.kt
@@ -1,10 +1,13 @@
 package com.pickdsm.pickserverspring.domain.classroom.spi
 
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
+import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
 import java.util.*
 
 interface QueryClassroomSpi {
     fun queryClassroomById(classroomId: UUID): Classroom
 
-    fun queryClassroomListByFloor(floor: Int): List<Classroom>
+    fun queryAllClassroomListByFloor(floor: Int): List<ClassroomElement>
+
+    fun querySelfStudyClassroomListByFloor(floor: Int): List<ClassroomElement>
 }
