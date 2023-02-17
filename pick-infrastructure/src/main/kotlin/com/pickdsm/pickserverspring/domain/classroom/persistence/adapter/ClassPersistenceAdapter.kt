bc25b4685d4e580ce2a5a277283df374bffbd933
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-17 16:32:29 +0900

    ♻️ :: 쿼리 하나로 합치기

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/adapter/ClassPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/adapter/ClassPersistenceAdapter.kt
@@ -1,14 +1,16 @@
 package com.pickdsm.pickserverspring.domain.classroom.persistence.adapter
 
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
+import com.pickdsm.pickserverspring.domain.classroom.ClassroomType
 import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
 import com.pickdsm.pickserverspring.domain.classroom.exception.ClassroomNotFoundException
 import com.pickdsm.pickserverspring.domain.classroom.persistence.ClassroomRepository
 import com.pickdsm.pickserverspring.domain.classroom.persistence.entity.QClassroomEntity.classroomEntity
 import com.pickdsm.pickserverspring.domain.classroom.spi.ClassroomSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
-import java.util.UUID
+import java.util.*
 
 @Adapter
 class ClassPersistenceAdapter(
@@ -20,26 +22,10 @@ class ClassPersistenceAdapter(
         classroomRepository.findClassroomEntityById(classroomId)
             ?: throw ClassroomNotFoundException
 
-    override fun queryAllClassroomListByFloor(floor: Int): List<ClassroomElement> =
+    override fun queryClassroomListByFloorAndByType(floor: Int, classroomType: String): List<ClassroomElement> =
         jpaQueryFactory
             .selectFrom(classroomEntity)
-            .where(classroomEntity.floor.eq(floor))
-            .fetch()
-            .map {
-                ClassroomElement(
-                    id = it.id,
-                    name = it.name,
-                    description = "",
-                )
-            }
-
-    override fun querySelfStudyClassroomListByFloor(floor: Int): List<ClassroomElement> =
-        jpaQueryFactory
-            .selectFrom(classroomEntity)
-            .where(
-                classroomEntity.floor.eq(floor),
-                classroomEntity.grade.isNotNull,
-            ) // 교실로 쓰이는 반은 학년이 존재
+            .where(checkClassroomType(classroomType), classroomEntity.floor.eq(floor))
             .fetch()
             .map {
                 ClassroomElement(
@@ -48,4 +34,11 @@ class ClassPersistenceAdapter(
                     description = "", // 교실은 별다른 설명 없음
                 )
             }
+
+    private fun checkClassroomType(classroomType: String): BooleanExpression? {
+        if (classroomType == ClassroomType.SELF_STUDY.name) {
+            return classroomEntity.grade.isNotNull
+        }
+        return null
+    }
 }
