986b2b0fe290aadfea9a3b8d64b30d0cbc8ece60
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-17 16:36:35 +0900

    ♻️ :: ktlint

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/QueryClassroomSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/spi/QueryClassroomSpi.kt
@@ -1,11 +1,11 @@
 package com.pickdsm.pickserverspring.domain.classroom.spi
 
 import com.pickdsm.pickserverspring.domain.classroom.Classroom
-import com.pickdsm.pickserverspring.domain.classroom.ClassroomType
 import com.pickdsm.pickserverspring.domain.classroom.api.dto.response.ClassroomElement
-import java.util.*
+import java.util.UUID
 
 interface QueryClassroomSpi {
+
     fun queryClassroomById(classroomId: UUID): Classroom
 
     fun queryClassroomListByFloorAndByType(floor: Int, classroomType: String): List<ClassroomElement>
