f95d48e733bcbc2dda0699aa0c5c3559ae8e58fd
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-17 18:02:17 +0900

    📑 :: ClassroomVO

---
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/persistence/vo/QueryClassroomVO.kt
@@ -0,0 +1,13 @@
+package com.pickdsm.pickserverspring.domain.classroom.persistence.vo
+
+import com.pickdsm.pickserverspring.domain.classroom.vo.ClassroomVO
+import com.querydsl.core.annotations.QueryProjection
+import java.util.UUID
+
+class QueryClassroomVO @QueryProjection constructor(
+    id: UUID,
+    name: String,
+) : ClassroomVO(
+    id = id,
+    name = name
+)
