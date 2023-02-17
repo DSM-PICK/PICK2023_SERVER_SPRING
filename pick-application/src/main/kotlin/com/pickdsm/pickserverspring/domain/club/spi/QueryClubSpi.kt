b911103468dea08dfcf47f57c77abcb477931b39
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-17 15:41:12 +0900

    📑 :: QueryClubSpi

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/club/spi/QueryClubSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/club/spi/QueryClubSpi.kt
@@ -1,8 +1,11 @@
 package com.pickdsm.pickserverspring.domain.club.spi
 
 import com.pickdsm.pickserverspring.domain.club.Club
+import java.util.UUID
 
 interface QueryClubSpi {
 
     fun queryClubList(): List<Club>
+
+    fun queryClubByClubId(clubId: UUID): Club?
 }
