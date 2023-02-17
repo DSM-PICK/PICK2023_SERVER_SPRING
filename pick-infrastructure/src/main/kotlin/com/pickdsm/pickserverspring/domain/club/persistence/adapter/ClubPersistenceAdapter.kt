e78d1b5b5346aa6bb1e2d04cb6ae256dc661474c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-17 12:18:07 +0900

    📑 :: ClubPersistenceAdapter

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/club/persistence/adapter/ClubPersistenceAdapter.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/club/persistence/adapter/ClubPersistenceAdapter.kt
@@ -1,15 +1,20 @@
 package com.pickdsm.pickserverspring.domain.club.persistence.adapter
 
 import com.pickdsm.pickserverspring.domain.club.Club
+import com.pickdsm.pickserverspring.domain.club.exception.ClubNotFoundException
 import com.pickdsm.pickserverspring.domain.club.mapper.ClubMapper
+import com.pickdsm.pickserverspring.domain.club.persistence.ClubRepository
 import com.pickdsm.pickserverspring.domain.club.persistence.entity.QClubEntity.clubEntity
 import com.pickdsm.pickserverspring.domain.club.spi.ClubSpi
 import com.pickdsm.pickserverspring.global.annotation.Adapter
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.repository.findByIdOrNull
+import java.util.UUID
 
 @Adapter
 class ClubPersistenceAdapter(
     private val clubMapper: ClubMapper,
+    private val clubRepository: ClubRepository,
     private val jpaQueryFactory: JPAQueryFactory,
 ) : ClubSpi {
 
@@ -18,4 +23,10 @@ class ClubPersistenceAdapter(
             .selectFrom(clubEntity)
             .fetch()
             .map(clubMapper::entityToDomain)
+
+    override fun changeClubHead(clubId: UUID, newHeadStudentId: UUID) {
+        val clubEntity = clubRepository.findByIdOrNull(clubId)
+            ?: throw ClubNotFoundException
+        clubEntity.changeClubHeadId(newHeadStudentId)
+    }
 }
