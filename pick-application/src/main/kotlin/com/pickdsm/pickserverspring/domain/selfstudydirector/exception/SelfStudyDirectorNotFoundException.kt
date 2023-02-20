a11f00332856e12b2bb1961706d6f3e9c51cf9f8
정대현 <eogus_2513@dsm.hs.kr>
2023-02-20 10:45:37 +0900

    ⚡️ :: Error 처리

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/exception/SelfStudyDirectorNotFoundException.kt
@@ -0,0 +1,8 @@
+package com.pickdsm.pickserverspring.domain.selfstudydirector.exception
+
+import com.pickdsm.pickserverspring.common.error.PickException
+import com.pickdsm.pickserverspring.domain.selfstudydirector.error.SelfStudyDirectorErrorCode
+
+object SelfStudyDirectorNotFoundException: PickException(
+    SelfStudyDirectorErrorCode.SELF_STUDY_DIRECTOR_NOT_FOUND
+)
