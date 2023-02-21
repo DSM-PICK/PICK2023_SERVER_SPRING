3fe8a6f4b53c1483d713ae0f4f4205eefc7708b2
lyutvs <lyutvs@gmail.com>
2023-02-21 15:26:06 +0900

    📑 :: DomainCahngeSelfStudtDirectorRequset

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/selfstudydirector/api/dto/requst/DomainChangeSelfStudyDirectorRequest.kt
@@ -0,0 +1,10 @@
+package com.pickdsm.pickserverspring.domain.selfstudydirector.api.dto.requst
+
+import java.time.LocalDate
+import java.util.UUID
+
+data class DomainChangeSelfStudyDirectorRequest(
+    val teacherId: UUID,
+    val floor: Int,
+    val date: LocalDate
+)
