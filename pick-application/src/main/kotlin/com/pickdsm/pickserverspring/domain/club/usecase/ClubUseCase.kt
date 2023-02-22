5861d379e0462b2b50c721871bb91cec465a0d07
min56 <minchaeg85@gmail.com>
2023-02-23 01:44:57 +0900

    ⚡️ :: changeClubStudentApi

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/club/usecase/ClubUseCase.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/club/usecase/ClubUseCase.kt
@@ -3,14 +3,17 @@ package com.pickdsm.pickserverspring.domain.club.usecase
 import com.pickdsm.pickserverspring.common.annotation.UseCase
 import com.pickdsm.pickserverspring.domain.club.api.ClubApi
 import com.pickdsm.pickserverspring.domain.club.api.dto.DomainChangeClubHeadRequest
+import com.pickdsm.pickserverspring.domain.club.api.dto.DomainChangeClubStudentRequest
 import com.pickdsm.pickserverspring.domain.club.exception.ClubNotFoundException
 import com.pickdsm.pickserverspring.domain.club.spi.CommandClubSpi
 import com.pickdsm.pickserverspring.domain.club.spi.QueryClubSpi
+import com.pickdsm.pickserverspring.domain.user.spi.UserSpi
 
 @UseCase
 class ClubUseCase(
     private val commandClubSpi: CommandClubSpi,
     private val queryClubSpi: QueryClubSpi,
+    private val userSpi: UserSpi,
 ) : ClubApi {
 
     override fun changeClubHead(request: DomainChangeClubHeadRequest) {
@@ -19,6 +22,15 @@ class ClubUseCase(
 
         commandClubSpi.saveClub(
             club.changeClubHead(headId = request.studentId),
+        )
+    }
+
+    override fun changeClubStudent(request: DomainChangeClubStudentRequest) {
+        val club = queryClubSpi.queryClubByClubId(request.clubId)
+            ?: throw ClubNotFoundException
+
+        commandClubSpi.saveClub(
+            club.changeClubStudent(clubId = request.clubId)
         )
     }
 }
