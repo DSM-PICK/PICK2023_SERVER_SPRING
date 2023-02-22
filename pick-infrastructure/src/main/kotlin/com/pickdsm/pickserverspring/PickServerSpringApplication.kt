303e12333a9f09fe3bd0aaee86b65ca3436e345e
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-22 14:59:05 +0900

    ♻️ :: @EnableScheduling 제거

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/PickServerSpringApplication.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/PickServerSpringApplication.kt
@@ -2,9 +2,7 @@ package com.pickdsm.pickserverspring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableScheduling
 @SpringBootApplication
 class PickServerSpringApplication
 
