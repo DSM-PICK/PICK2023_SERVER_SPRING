f64cb11b3d5ad6ba86a362967b54d1c62cbc04bb
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-22 14:59:28 +0900

    📑 :: SchedulerConfig

---
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/global/config/SchedulerConfig.kt
@@ -0,0 +1,8 @@
+package com.pickdsm.pickserverspring.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@EnableScheduling
+@Configuration
+class SchedulerConfig
