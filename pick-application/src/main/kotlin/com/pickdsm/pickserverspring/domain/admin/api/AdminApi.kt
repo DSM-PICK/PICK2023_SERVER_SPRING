38c626c5ae02cfb20a524b29ef2dd8f76dc423c6
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-26 19:06:22 +0900

    📑 :: AdminApi

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/admin/api/AdminApi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/admin/api/AdminApi.kt
@@ -1,8 +1,13 @@
 package com.pickdsm.pickserverspring.domain.admin.api
 
 import com.pickdsm.pickserverspring.domain.admin.api.dto.request.DomainUpdateStudentStatusOfClassRequest
+import com.pickdsm.pickserverspring.domain.admin.api.dto.response.QueryStudentAttendanceList
+import java.time.LocalDate
+import java.util.UUID
 
 interface AdminApi {
 
     fun updateStudentStatusOfClass(request: DomainUpdateStudentStatusOfClassRequest)
+
+    fun getStudentAttendanceList(classroomId: UUID, date: LocalDate): QueryStudentAttendanceList
 }
