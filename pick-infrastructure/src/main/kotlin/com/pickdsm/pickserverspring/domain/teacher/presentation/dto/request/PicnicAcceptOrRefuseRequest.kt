74c7316380eb51726be22e201ce5ea140f86d2c4
min56 <minchaeg85@gmail.com>
2023-02-26 11:08:16 +0900

    📑 :: PicnicAcceptOrRefuseRequest

---
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/domain/teacher/presentation/dto/request/PicnicAcceptOrRefuseRequest.kt
@@ -0,0 +1,14 @@
+package com.pickdsm.pickserverspring.domain.teacher.presentation.dto.request
+
+import com.pickdsm.pickserverspring.domain.application.StatusType
+import java.util.*
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+data class PicnicAcceptOrRefuseRequest(
+    @field: NotBlank
+    val type: StatusType,
+
+    @field: NotNull
+    val userIdList: List<UUID>,
+)
