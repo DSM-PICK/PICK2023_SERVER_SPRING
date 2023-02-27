40921b4869ba33b614ce85613e912e4935345258
min56 <minchaeg85@gmail.com>
2023-02-27 15:41:01 +0900

    ♻️ :: 클래스명 변경

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/admin/api/dto/response/QueryTypeResponse.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/admin/api/dto/response/QueryTypeResponse.kt
@@ -3,7 +3,7 @@ package com.pickdsm.pickserverspring.domain.admin.api.dto.response
 import com.pickdsm.pickserverspring.domain.selfstudydirector.DirectorType
 import java.time.LocalDate
 
-data class QueryTodayTypeResponse(
+data class QueryTypeResponse(
     val date: LocalDate,
 
     val type: DirectorType,
