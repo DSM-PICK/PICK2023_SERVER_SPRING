d67f91e66a731598cc549f435774cfe00c49676c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-03 23:12:18 +0900

    ⚡️ :: QueryResponsibleClassroomList

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/api/dto/response/QueryResponsibleClassroomList.kt
@@ -0,0 +1,14 @@
+package com.pickdsm.pickserverspring.domain.classroom.api.dto.response
+
+import java.util.UUID
+
+data class QueryResponsibleClassroomList(
+    val floor: Int,
+    val responsibleClassroomList: List<ResponsibleClassroomElement>,
+) {
+    data class ResponsibleClassroomElement(
+        val id: UUID,
+        val name: String,
+        val description: String,
+    )
+}
