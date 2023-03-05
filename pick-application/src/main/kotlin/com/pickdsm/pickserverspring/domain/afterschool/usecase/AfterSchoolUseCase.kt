f876ba228e2bf5fe432f7e1c75c23514e98bbc20
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-05 16:33:26 +0900

    ⚡️ :: 방과후 - 인원 조회 메소드 추가

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/afterschool/usecase/AfterSchoolUseCase.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/afterschool/usecase/AfterSchoolUseCase.kt
@@ -5,15 +5,21 @@ import com.pickdsm.pickserverspring.domain.afterschool.AfterSchool
 import com.pickdsm.pickserverspring.domain.afterschool.api.AfterSchoolApi
 import com.pickdsm.pickserverspring.domain.afterschool.api.dto.request.DomainCreateAfterSchoolStudentRequest
 import com.pickdsm.pickserverspring.domain.afterschool.api.dto.request.DomainDeleteAfterSchoolStudentRequest
+import com.pickdsm.pickserverspring.domain.afterschool.api.dto.response.QueryAfterSchoolStudentList
+import com.pickdsm.pickserverspring.domain.afterschool.api.dto.response.QueryAfterSchoolStudentList.QueryAfterSchoolStudentElement
 import com.pickdsm.pickserverspring.domain.afterschool.exception.AfterSchoolNotFoundException
 import com.pickdsm.pickserverspring.domain.afterschool.exception.AfterSchoolStudentNotFoundException
 import com.pickdsm.pickserverspring.domain.afterschool.spi.CommandAfterSchoolSpi
 import com.pickdsm.pickserverspring.domain.afterschool.spi.QueryAfterSchoolSpi
+import com.pickdsm.pickserverspring.domain.user.exception.UserNotFoundException
+import com.pickdsm.pickserverspring.domain.user.spi.UserSpi
+import java.util.UUID
 
 @UseCase
 class AfterSchoolUseCase(
-    private val queryAfterSchoolSpi: QueryAfterSchoolSpi,
     private val commandAfterSchoolSpi: CommandAfterSchoolSpi,
+    private val queryAfterSchoolSpi: QueryAfterSchoolSpi,
+    private val userSpi: UserSpi,
 ) : AfterSchoolApi {
 
     override fun deleteAfterSchoolStudent(domainDeleteAfterSchoolStudentRequest: DomainDeleteAfterSchoolStudentRequest) {
@@ -45,4 +51,35 @@ class AfterSchoolUseCase(
         }
         commandAfterSchoolSpi.saveAll(afterSchools)
     }
+
+    override fun getAfterSchoolStudents(afterSchoolId: UUID): QueryAfterSchoolStudentList {
+        val afterSchool = queryAfterSchoolSpi.findByAfterSchoolId(afterSchoolId)
+            ?: throw AfterSchoolNotFoundException
+        val afterSchoolList = queryAfterSchoolSpi.queryAfterSchoolListByAfterSchoolId(afterSchoolId)
+        val afterSchoolStudentIdList = afterSchoolList.map { it.studentId }
+        val afterSchoolStudentInfos = userSpi.queryUserInfo(afterSchoolStudentIdList)
+
+        val afterSchoolUsers = afterSchoolList.map {
+            val user = afterSchoolStudentInfos.find { user -> user.id == it.studentId }
+                ?: throw UserNotFoundException
+
+            QueryAfterSchoolStudentElement(
+                studentId = user.id,
+                studentNumber = "${user.grade}${user.classNum}${checkUserNumLessThanTen(user.num)}",
+                studentName = user.name,
+            )
+        }
+
+        return QueryAfterSchoolStudentList(
+            afterSchoolName = afterSchool.afterSchoolName,
+            afterSchoolUserList = afterSchoolUsers,
+        )
+    }
+
+    private fun checkUserNumLessThanTen(userNum: Int) =
+        if (userNum < 10) {
+            "0$userNum"
+        } else {
+            userNum.toString()
+        }
 }
