61d03ba392ff0578c75173a431c4dc905e98a0d8
seongwon <seongwon1290@dsm.hs.kr>
2023-03-08 22:45:16 +0900

    ♻️ :: 자퇴 추가

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/StatusType.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/StatusType.kt
@@ -3,4 +3,5 @@ package com.pickdsm.pickserverspring.domain.application
 enum class StatusType {
     DISALLOWED, LEAVE, HOME, FIELD_TRIP, EMPLOYMENT, PICNIC, MOVEMENT, AWAIT,
     PICNIC_REJECT, ATTENDANCE, NOT_COMEBACK, FIELD_TRIP_START, EMPLOYMENT_START,
+    DROPOUT
 }
