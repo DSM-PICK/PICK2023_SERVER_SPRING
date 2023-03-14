5cb4b58990cef2b57a36d177815fbd7b65f950c7
jaemin <sonjm3518@dsm.hs.kr>
2023-03-14 16:05:14 +0900

    📑 :: 교실이 없을 경우 404 에러처리

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/error/ClassroomErrorCode.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/error/ClassroomErrorCode.kt
@@ -8,7 +8,8 @@ enum class ClassroomErrorCode(
 ) : ErrorProperty {
 
     CLASS_NOT_FOUND(404, "Class not found"),
-    FLOOR_NOT_FOUND(404, "Floor not found"), ;
+    FLOOR_NOT_FOUND(404, "Floor not found"),
+    CLASSROOM_MOVEMENT_STUDENT_NOT_FOUND(404, "Classroom Movement Student Not Found"), ;
 
     override fun status(): Int = status
     override fun message(): String = message
