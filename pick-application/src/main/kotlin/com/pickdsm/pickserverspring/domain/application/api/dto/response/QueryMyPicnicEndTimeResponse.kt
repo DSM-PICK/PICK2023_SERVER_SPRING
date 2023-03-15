7aeeed253630ac2dd2bd245a8e5b8cc57a20477e
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-15 21:03:41 +0900

    ♻️ :: QueryMyPicnicEndTimeResponse

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/api/dto/response/QueryMyPicnicEndTimeResponse.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/api/dto/response/QueryMyPicnicEndTimeResponse.kt
@@ -3,7 +3,7 @@ package com.pickdsm.pickserverspring.domain.application.api.dto.response
 import java.time.LocalTime
 import java.util.UUID
 
-data class QueryMyPicnicResponse(
+data class QueryMyPicnicEndTimeResponse(
     val userId: UUID,
     val name: String,
     val endTime: LocalTime,
