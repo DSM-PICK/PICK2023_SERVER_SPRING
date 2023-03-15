7460ba63dd032c42db2803b94b39cc1a95db9df8
min56 <minchaeg85@gmail.com>
2023-03-15 18:39:15 +0900

    📑 :: QueryClassroomMovementLocationResponse

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/api/dto/response/QueryClassroomMovementLocationResponse.kt
@@ -0,0 +1,6 @@
+package com.pickdsm.pickserverspring.domain.classroom.api.dto.response
+
+data class QueryClassroomMovementLocationResponse(
+    val name: String,
+    val locationClassroom: String,
+)
