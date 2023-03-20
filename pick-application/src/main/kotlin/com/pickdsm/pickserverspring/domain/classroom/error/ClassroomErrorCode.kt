0969ce72a1058ac1a082255732e9122c2f55a7a1
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-20 17:35:40 +0900

    ⚡️ :: CannotMovementException 추가

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/error/ClassroomErrorCode.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/error/ClassroomErrorCode.kt
@@ -7,6 +7,8 @@ enum class ClassroomErrorCode(
     private val message: String,
 ) : ErrorProperty {
 
+    CANNOT_MOVEMENT(400, "Cannot Movement"),
+
     CLASS_NOT_FOUND(404, "Class not found"),
     FLOOR_NOT_FOUND(404, "Floor not found"),
     CLASSROOM_MOVEMENT_STUDENT_NOT_FOUND(404, "Classroom Movement Student Not Found"), ;
