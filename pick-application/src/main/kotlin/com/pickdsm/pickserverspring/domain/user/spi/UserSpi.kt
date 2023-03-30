5c76ebcb1955a6b794cac2d4ab58e8e7a928ed51
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-30 19:16:36 +0900

    📑 :: UserSpi

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/user/spi/UserSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/user/spi/UserSpi.kt
@@ -3,8 +3,12 @@ package com.pickdsm.pickserverspring.domain.user.spi
 import com.pickdsm.pickserverspring.domain.application.spi.UserQueryApplicationSpi
 import com.pickdsm.pickserverspring.domain.selfstudydirector.spi.UserQuerySelfStudyDirectorSpi
 import com.pickdsm.pickserverspring.domain.teacher.spi.UserQueryTeacherSpi
+import com.pickdsm.pickserverspring.domain.user.User
+import com.pickdsm.pickserverspring.domain.user.dto.UserInfo
 import java.util.UUID
 
 interface UserSpi : UserQueryApplicationSpi, UserQuerySelfStudyDirectorSpi, UserQueryTeacherSpi {
     fun getCurrentUserId(): UUID
+
+    fun getAllUserInfo(): List<User>
 }
