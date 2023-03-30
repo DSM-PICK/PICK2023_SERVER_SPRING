033521b1c760af8189f960b99b3d80b5c459c57a
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-30 19:31:36 +0900

    ♻️ :: ktlint

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/user/spi/UserSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/user/spi/UserSpi.kt
@@ -4,7 +4,6 @@ import com.pickdsm.pickserverspring.domain.application.spi.UserQueryApplicationS
 import com.pickdsm.pickserverspring.domain.selfstudydirector.spi.UserQuerySelfStudyDirectorSpi
 import com.pickdsm.pickserverspring.domain.teacher.spi.UserQueryTeacherSpi
 import com.pickdsm.pickserverspring.domain.user.User
-import com.pickdsm.pickserverspring.domain.user.dto.UserInfo
 import java.util.UUID
 
 interface UserSpi : UserQueryApplicationSpi, UserQuerySelfStudyDirectorSpi, UserQueryTeacherSpi {
