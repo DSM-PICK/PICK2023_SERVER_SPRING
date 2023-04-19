f8dd0775e96879e7ed649019b7f3137ea166d2dc
seongwon <seongwon1290@dsm.hs.kr>
2023-04-19 09:15:42 +0900

    ♻️ :: JsonFormat 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/feign/client/dto/response/ScheduleResponse.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/feign/client/dto/response/ScheduleResponse.kt
@@ -1,5 +1,9 @@
 package com.pickdsm.pickserverspring.common.feign.client.dto.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ScheduleResponse(
+
+    @JsonProperty("isHomecomingDay")
     val isHomecomingDay: Boolean,
 )
