c1da041dd6a4a717cbe2c99724e91d9123e188f1
min56 <minchaeg85@gmail.com>
2023-04-24 16:14:04 +0900

    📑 :: CannotMovementWeekendException

---
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/exception/CannotMovementWeekendException.kt
@@ -0,0 +1,8 @@
+package com.pickdsm.pickserverspring.domain.classroom.exception
+
+import com.pickdsm.pickserverspring.common.error.PickException
+import com.pickdsm.pickserverspring.domain.classroom.error.ClassroomErrorCode
+
+object CannotMovementWeekendException : PickException (
+    ClassroomErrorCode.CANNOT_MOVEMEMENT_WEEKEND
+)
