614cb9fba16365092482609d55cbb3b446c764af
min56 <minchaeg85@gmail.com>
2023-05-17 17:25:13 +0900

    ♻️ :: Ktlint 해결

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/exception/AfterSchoolCannotMovementException.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/classroom/exception/AfterSchoolCannotMovementException.kt
@@ -3,6 +3,6 @@ package com.pickdsm.pickserverspring.domain.classroom.exception
 import com.pickdsm.pickserverspring.common.error.PickException
 import com.pickdsm.pickserverspring.domain.classroom.error.ClassroomErrorCode
 
-object AfterSchoolCannotMovementException: PickException(
-    ClassroomErrorCode.AFTER_SCHOOL_CANNOT_MOVEMENT
+object AfterSchoolCannotMovementException : PickException(
+    ClassroomErrorCode.AFTER_SCHOOL_CANNOT_MOVEMENT,
 )
