2f50742c5461eb5ac9754daf22ba5eeec6712d61
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-05-26 21:55:43 +0900

    ♻️ :: 안 쓰는 메소드 제거

--- pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/spi/QueryApplicationSpi.kt
+++ pick-application/src/main/kotlin/com/pickdsm/pickserverspring/domain/application/spi/QueryApplicationSpi.kt
@@ -1,14 +1,11 @@
 package com.pickdsm.pickserverspring.domain.application.spi
 
 import com.pickdsm.pickserverspring.domain.application.Application
-import java.time.LocalDate
 import java.util.UUID
 
 interface QueryApplicationSpi {
 
-    fun queryPicnicApplicationListByToday(date: LocalDate): List<Application>
-
-    fun queryApplicationListByToday(date: LocalDate): List<Application>
+    fun queryPicnicApplicationListByToday(): List<Application>
 
     fun queryApplicationByStudentIdAndStatusId(studentId: UUID, statusId: UUID): Application?
 }
