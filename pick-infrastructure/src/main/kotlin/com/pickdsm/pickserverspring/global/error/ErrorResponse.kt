349e11750ef7f11ddf8c98daa15d65ab4aa1451e
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-06-15 09:25:58 +0900

    ♻️ :: 확장함수로 분리

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/global/error/ErrorResponse.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/global/error/ErrorResponse.kt
@@ -7,43 +7,38 @@ import org.springframework.validation.FieldError
 
 data class ErrorResponse(
     val status: Int,
-    val message: String
-) {
-
-    companion object {
-        fun of(errorProperty: ErrorProperty) = ErrorResponse(
-            errorProperty.status(),
-            errorProperty.message()
-        )
-
-        fun of(e: BindingResult): ValidationErrorResponse {
-            val errorMap = HashMap<String, String?>()
-
-            for (error: FieldError in e.fieldErrors) {
-                errorMap[error.field] = error.defaultMessage
-            }
-
-            return ValidationErrorResponse(
-                status = GlobalErrorCode.BAD_REQUEST.status(),
-                fieldError = errorMap
-            )
-        }
-
-        fun of(e: DataIntegrityViolationException): DataErrorResponse {
-            return DataErrorResponse(
-                status = GlobalErrorCode.BAD_REQUEST.status(),
-                message = e.message.toString()
-            )
-        }
-    }
-}
+    val message: String,
+)
 
 data class ValidationErrorResponse(
     val status: Int,
-    val fieldError: Map<String, String?>
+    val fieldError: Map<String, String?>,
 )
 
-data class DataErrorResponse(
-    val status: Int,
-    val message: String
+fun ErrorProperty.of() = ErrorResponse(
+    this.status(),
+    this.message()
+)
+
+fun BindingResult.of(): ValidationErrorResponse {
+    val errorMap = HashMap<String, String?>()
+
+    for (error: FieldError in this.fieldErrors) {
+        errorMap[error.field] = error.defaultMessage
+    }
+
+    return ValidationErrorResponse(
+        status = GlobalErrorCode.BAD_REQUEST.status(),
+        fieldError = errorMap
+    )
+}
+
+fun DataIntegrityViolationException.of() = ErrorResponse(
+    status = GlobalErrorCode.BAD_REQUEST.status(),
+    message = this.message.toString(),
+)
+
+fun IllegalArgumentException.of() = ErrorResponse(
+    status = GlobalErrorCode.BAD_REQUEST.status(),
+    message = this.message.toString()
 )
