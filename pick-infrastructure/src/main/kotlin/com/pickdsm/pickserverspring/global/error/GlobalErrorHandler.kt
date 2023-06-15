c321fa98da9d8e766a4955ba48210a501f1e0c94
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-06-15 09:25:39 +0900

    ⚡️ :: IllegalArgumentException 핸들링 추가

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/global/error/GlobalErrorHandler.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/global/error/GlobalErrorHandler.kt
@@ -7,19 +7,24 @@ import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.lang.IllegalArgumentException
 
 @RestControllerAdvice
 class GlobalErrorHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException::class)
-    protected fun handleBindException(e: BindException): ValidationErrorResponse? = ErrorResponse.of(e)
+    protected fun handleBindException(e: BindException): ValidationErrorResponse? = e.of()
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    protected fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ValidationErrorResponse? = ErrorResponse.of(e)
+    protected fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ValidationErrorResponse? = e.of()
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DataIntegrityViolationException::class)
-    protected fun handleDataIntegrityViolationException(e: DataIntegrityViolationException): DataErrorResponse? = ErrorResponse.of(e)
+    protected fun handleDataIntegrityViolationException(e: DataIntegrityViolationException): ErrorResponse? = e.of()
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException::class)
+    protected fun handleIllegalArgumentException(e: IllegalArgumentException): ErrorResponse? = e.of()
 }
