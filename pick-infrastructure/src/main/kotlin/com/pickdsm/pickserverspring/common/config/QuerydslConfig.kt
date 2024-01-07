0e7f142bcfe3e888f75291c9c4edc349445b3a2e
이정호 <dlwjdgh1115@gmail.com>
2024-01-07 19:35:17 +0900

    ♻️ :: config 패키지 통합

--- pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/config/QuerydslConfig.kt
+++ pick-infrastructure/src/main/kotlin/com/pickdsm/pickserverspring/common/config/QuerydslConfig.kt
@@ -1,4 +1,4 @@
-package com.pickdsm.pickserverspring.common.querydsl
+package com.pickdsm.pickserverspring.common.config
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.context.annotation.Bean
